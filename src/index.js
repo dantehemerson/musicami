43db31d9c7b5747ce0a4c600344fb153ed92cd5d
Dante Calderón <dantehemerson@gmail.com>
2018-09-04 06:43:45 -0500

    Basic structure with redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import Root from './root'
+import registerServiceWorker from './registerServiceWorker'
+import reducers from './reducers'
+import './static/css/index.css'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+	reducers,
+	composeEnhancers(
+		thunk
+	)
+)
+
+ReactDOM.render(<Root store={ store } />, document.getElementById('root'))
+registerServiceWorker()
