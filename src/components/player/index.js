93b5b2ca7f43ac0ca1a5723a249eb6f001938aca
Dante Calderón <dantehemerson@gmail.com>
2018-09-08 09:50:08 -0500

    :bug: Fix bug where add to up next plays immediately

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -22,6 +22,7 @@ export default class Player extends React.Component {
 		api.fetchStreamURL(newProps.currentSong.stream_url)
 			.then(url => this.setState({...this.state, streamURL: url}))
 		this.props.getSuggestions(newProps.currentSong.suggest_url)
+		this.props.playedNext()
 	}
 
 	render() {
