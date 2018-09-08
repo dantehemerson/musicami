5f0582c335d87881421b8f9ad65448af6f978697
Dante Calderón <dantehemerson@gmail.com>
2018-09-08 00:43:50 -0500

    :evergreen_tree: Add feature to play next song after current song is finished
    Sidebar actions for play next song.

--- src/components/sideBar/index.js
+++ src/components/sideBar/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
 
 export default class SideBar extends React.Component {
+	componentWillReceiveProps(nextProps) {
+		if(nextProps.dispatchNext) {
+			this.props.playedNext()
+			if(this.props.videos.length > 0) {
+				let video = this.props.video[0]
+				this.props.removeVideo(video)
+				this.props.playSong(video)
+			}
+		}
+	}
 	render() {
 		return (
 			<div>
