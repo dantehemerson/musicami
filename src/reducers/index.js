e21aa2356640d9cc54a8ecaaa1cb2f591787a0c3
Dante Calderón <dantehemerson@gmail.com>
2018-09-07 23:41:20 -0500

    Player reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import { navigationHome } from './navigationHomeReducer'
 import { search } from './searchReducer'
 import { nowPlaying } from './nowPlayingReducer'
+import { player } from './playerReducer'
 
 export default combineReducers({ 
 	navigationHome, 
 	search,
-	nowPlaying
+	nowPlaying,
+	player
 })
---
+++ src/reducers/playerReducer.js
@@ -0,0 +1,17 @@
+import { actionTypes } from '../actions/playerActions'
+
+const initialState = {
+	currentSong: null
+}
+
+export function player(state = initialState, action) {
+	switch(action.type) {
+		case actionTypes.playSong:
+			return {
+				...state,
+				currentSong: action.video
+			}
+		default:
+			return state
+	}
+}
