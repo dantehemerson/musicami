fe213c0bbb024bc6b4cab5836579fe86e0f6b8b7
Dante Calderón <dantehemerson@gmail.com>
2018-09-08 07:18:18 -0500

    :sparkles: Pass trending reducer to store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import { navigationHome } from './navigationHomeReducer'
 import { search } from './searchReducer'
 import { nowPlaying } from './nowPlayingReducer'
 import { player } from './playerReducer'
+import { trending } from './trendingReducer'
 
 export default combineReducers({ 
 	navigationHome, 
 	search,
 	nowPlaying,
-	player
+	player,
+	trending
 })
