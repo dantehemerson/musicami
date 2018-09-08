2beacfc1e0ac838e55a46a566f5c71465bece959
Dante Calderón <dantehemerson@gmail.com>
2018-09-08 05:37:27 -0500

    :sparkles: Add trending actions

--- src/utils/api.js
+++ src/utils/api.js
@@ -21,5 +21,18 @@ export default {
 			fetch(host + suggestUrl)
 				.then(response => response.json().then(data => resolve(data.results)))
 		})
+	},
+	getPlaylists: () => {
+		return new Promise((resolve, reject) => {
+			fetch(host + '/api/v1/playlists')
+				.then(response => response.json().then(data => resolve(data.results)))
+		})
+	},
+	getPlaylistsSong: (playlists, count) => {
+		let commaSeparatedPlaylists = playlists.map(a => a.playlist).join()
+		return new Promise((resolve, reject) => {
+			fetch(host + '/api/v1/trending?' + getQueryString({ type: commaSeparatedPlaylists, number: count }))
+				.then(response => response.json().then(data => resolve(data.results)))
+		})
 	}
 }
