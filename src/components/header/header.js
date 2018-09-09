347c684379c8ebc691e8b670e21d821bc5d83e5f
Dante Calderón <dantehemerson@gmail.com>
2018-09-09 12:03:29 -0500

    :bug: Fixed header height

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -35,12 +35,12 @@ class Header extends React.Component {
          <div>
             <header className="app-header">
                <div className="uk-container">
-                  <nav className="uk-navbar uk-navbar-container uk-navbar-transparent">
+                  <nav className="uk-navbar uk-navbar-container uk-navbar-transparent navbar-custom">
                      <div className="uk-navbar-left">                       
                         <Link className="uk-navbar-item uk-logo uk-visible@s" to='/'><img src={Logo} alt="Musicami"/></Link>
                      </div>
                      <div className="anyaudio-search-container uk-navbar-left uk-margin-medium-left uk-margin-medium-right uk-nav-center-sm">
-                     
+
                   {/* action="javascript:void(0) */}
                         <form className="uk-search uk-search-default anyaudio-search" 
 
