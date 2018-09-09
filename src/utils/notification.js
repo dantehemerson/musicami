40205c4183879ec7e4b6eaa2b57e58ed79e7b643
Dante Calderón <dantehemerson@gmail.com>
2018-09-08 19:25:53 -0500

    :dizzy: Improve notification

--- src/utils/notification.js
+++ src/utils/notification.js
@@ -1,8 +1,11 @@
-export default (title) => {
+export default (title, body) => {
 	Notification
 		.requestPermission()
 		.then(function() {
-			let n = new Notification(title)
+			let n = new Notification(title, {
+				body,
+				icon: '/favico.ico'
+			})
 			setTimeout(n.close.bind(n), 2000)
 		})
 }
