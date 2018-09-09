ed097224a8fd3171b4173d2231ccd0ba217e1ac7
Dante Calderón <dantehemerson@gmail.com>
2018-09-09 08:35:19 -0500

    :heavy_plus_sign: Add constants

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -36,7 +36,7 @@ class Header extends React.Component {
                <div className="uk-container">
                   <nav className="uk-navbar uk-navbar-container uk-navbar-transparent">
                      <div className="uk-navbar-left">                       
-                        <Link className="uk-navbar-item uk-logo uk-visible@s" to='/'><img src={Logo} alt="Musicami"></img></Link>
+                        <Link className="uk-navbar-item uk-logo uk-visible@s" to='/'><img src={Logo} alt="Musicami"/></Link>
                      </div>
                      <div className="anyaudio-search-container uk-navbar-left uk-margin-medium-left uk-margin-medium-right uk-nav-center-sm">
                         <form className="uk-search uk-search-default anyaudio-search" action="javascript:void(0)" 
