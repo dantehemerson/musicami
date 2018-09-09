3652b3cef1f733cacc3fe31e036cb3e63d1e5f33
Dante Calderón <dantehemerson@gmail.com>
2018-09-09 05:04:08 -0500

    :nail_care: Import UIKit components and use their icons

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ import Root from './root'
 import registerServiceWorker from './registerServiceWorker'
 import reducers from './reducers'
 
+import Icons from 'uikit/dist/js/uikit-icons'
+import UIKit from 'uikit'
+
+import 'uikit/dist/css/uikit.min.css'
+
+UIKit.use(Icons)
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
