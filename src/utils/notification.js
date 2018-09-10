c2acc8b8b49631bd3aff27cc8a45abffb48a268a
Dante Calderón <dantehemerson@gmail.com>
2018-09-10 03:46:28 -0500

    :bug: Fix icon notification and Change the time to 3s

--- src/utils/notification.js
+++ src/utils/notification.js
@@ -1,11 +1,13 @@
+import Logo from '../components/header/static/img/logo.png'
+
 export default (title, body) => {
 	Notification
 		.requestPermission()
 		.then(function() {
 			let n = new Notification(title, {
 				body,
-				icon: '/favico.ico'
+				icon: Logo
 			})
-			setTimeout(n.close.bind(n), 2000)
+			setTimeout(n.close.bind(n), 3000)
 		})
 }
