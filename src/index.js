ea4e419399b11791485c0933182b9e8a355bfcb4
dantehemerson <dantehemerson@gmail.com>
2018-11-11 06:52:54 -0500

    Remove uikit

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
 import React from 'react'
 import { hydrate, render } from 'react-dom'
 import { createStore, compose, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
+import thunkMiddleware from 'redux-thunk'
 
 import Root from './root'
 import registerServiceWorker from './registerServiceWorker'
-import reducers from './reducers'
-
-import Icons from 'uikit/dist/js/uikit-icons'
-import UIKit from 'uikit'
-
-import 'uikit/dist/css/uikit.min.css'
-
-UIKit.use(Icons)
+import reducer from './reducers'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
-	reducers,
+	reducer,
 	composeEnhancers(
-		applyMiddleware(thunk)
+		applyMiddleware(thunkMiddleware)
 	)
 )
 
