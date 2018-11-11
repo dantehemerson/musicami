aaea09a491cde78352170dab2337550b56ba3afb
dantehemerson <dantehemerson@gmail.com>
2018-11-11 07:14:22 -0500

    Fix Sidebar

--- src/root.js
+++ src/root.js
@@ -5,17 +5,15 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { css } from 'react-emotion'
 
 import './static/css/root.css'
+import 'semantic-ui-css/semantic.min.css'
 
-// containers
 import Header from './containers/headerContainer'
 import NavigationHome from './containers/navigation/navigationHomeContainer'
 import NavigationSearch from './containers/navigation/navigationSearchContainer'
 import Explore from './containers/Explore'
 import Channel from './containers/Channel'
 import Player from './containers/playerContainer'
-//import Sidebar from './containers/sideBarContainer'
-import 'semantic-ui-css/semantic.min.css';
-import SidebarMenu from './components/SidebarMenu'
+import Sidebar from './components/Sidebar'
 import NotFound from './containers/NotFound'
 
 const Root = ({ store }) => (
@@ -23,7 +21,7 @@ const Root = ({ store }) => (
 		<BrowserRouter basename="/musicami">
       <div>
         <Header/>
-        <SidebarMenu/>
+        <Sidebar/>
       	<div
           className={css`
             margin-left: 220px !important;
@@ -36,11 +34,6 @@ const Root = ({ store }) => (
             <Route exact path='/search/:q' component={ NavigationSearch }/>
             <Route component={NotFound}/>
           </Switch>
-          {/*
-          <div className="sidebar-container">
-            <Sidebar />
-          </div>
-        */}
         </div>
   			<div className="page-bottom">
           <Player/>
