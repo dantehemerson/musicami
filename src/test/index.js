be7c7505ef9a5d45408a915b406e2376d5b5c81e
dantehemerson <dantehemerson@gmail.com>
2018-11-13 01:15:10 -0500

    Get data

--- src/test/index.js
+++ src/test/index.js
@@ -1,7 +1,21 @@
 const  { normalize, schema } = require('normalizr')
+const fetch = require('node-fetch')
 
 const host = 'https://api.anyaudio.in'
 
+const getQueryString = (params) => {
+  return Object
+    .keys(params)
+    .map(k => {
+      if(Array.isArray(params[k])) {
+        return params[k]
+          .map(val => `${encodeURIComponent(k)}[]=${encodeURIComponent(val)}`)
+          .join('&')
+      }
+      return `${encodeURIComponent(k)}=${encodeURIComponent(params[k])}`
+    })
+    .join('&')
+}
 
 const getPlaylists = () => {
   return new Promise((resolve, reject) => {
@@ -10,5 +24,21 @@ const getPlaylists = () => {
   })
 }
 
-getPlaylists()
-  .then(res => console.log(res))
+const getPlaylistSongs = (playlists, count) => {
+  if(Array.isArray(playlists)) {
+    playlists = playlists.join()
+  }
+  return new Promise((resolve, reject) => {
+    fetch(host + '/api/v1/trending?' + getQueryString({
+      type: playlists,
+      number: count
+    })).then(response => response.json().then(data => resolve(data.results)))
+  })
+}
+
+getPlaylistSongs('Popular', 3)
+.then(data => {
+
+
+
+})
